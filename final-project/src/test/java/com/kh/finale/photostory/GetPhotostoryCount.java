a22bc17ca82425f5e6d26be9d896177581b3fb91
swjk78 <swjk78@gmail.com>
2021-07-12 16:59:43 +0900

    테스트 소스 업로드
    - 총 포토스토리 개수 구하기 테스트
    - 포토스토리 리스트 구하기 테스트
    - 포토스토리 작성 테스트

---
+++ final-project/src/test/java/com/kh/finale/photostory/GetPhotostoryCount.java
@@ -0,0 +1,47 @@
+package com.kh.finale.photostory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.kh.finale.repository.PhotostoryDao;
+import com.kh.finale.vo.PhotostoryVO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+})
+@WebAppConfiguration
+@Slf4j
+/**
+ * 총 포토스토리 개수 구하기 테스트
+ * @author swjk78
+ */
+public class GetPhotostoryCount {
+
+	@Autowired
+	private PhotostoryDao photostoryDao;
+
+	@Test
+	public void test() {
+		PhotostoryVO photostoryVO = PhotostoryVO.builder()
+				.startRow(1)
+				.endRow(10)
+				.pageNo(1)
+				.pageSize(10)
+				.searchType(null)
+				.searchKeyword(null)
+				.startBlock(1)
+				.endBlock(1)
+				.lastBlock(1)
+				.build();
+		int count = photostoryDao.getPhotostoryCount(photostoryVO);
+		log.debug("count = {}", count);
+	}
+}
