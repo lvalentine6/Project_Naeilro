3ba3378ac0aebc4da4ab6d694a9ff7318917ca7e
swjk78 <swjk78@gmail.com>
2021-07-16 14:50:16 +0900

    주석 위치 변경

--- final-project/src/main/java/com/kh/finale/vo/photostory/PhotostoryListVO.java
+++ final-project/src/main/java/com/kh/finale/vo/photostory/PhotostoryListVO.java
@@ -5,14 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 /**
  * 포토스토리 리스트 나열을 위한 파라미터 모음
  * @author swjk78
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PhotostoryListVO {
 	private int startRow, endRow, pageNo, pageSize;
 	private String searchType, searchKeyword;
