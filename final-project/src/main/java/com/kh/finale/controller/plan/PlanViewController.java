2f75643efed92444d921979459b5648a2758eeef
swjk78 <swjk78@gmail.com>
2021-08-03 01:57:02 +0900

    계획표 수정 페이지 구현 중

--- final-project/src/main/java/com/kh/finale/controller/plan/PlanViewController.java
+++ final-project/src/main/java/com/kh/finale/controller/plan/PlanViewController.java
@@ -67,7 +67,6 @@ public class PlanViewController {
 			int dailyplanCount = dailyplanDao.getDailyplanCount(vo.getDailyNo());
 			dailyplanCountList.add(dailyplanCount);
 		}
-		System.out.println("???="+dailyplanCountList);
 		
 		model.addAttribute("dailyplanCountList", dailyplanCountList);
 		
